e7eee42fda8b23244699f066b1c0868ad5e4f2f5
hri-kiro <122716156+hri-kiro@users.noreply.github.com>
2024-10-28 16:40:05 +0900

    Update MyServer_realsense.py

--- MyServer_realsense.py
+++ MyServer_realsense.py
@@ -24,7 +24,7 @@ class RealSenseRecorder:
     def start_recording(self, click_time, message):
         # 클릭시간과 메시지를 포함한 파일이름 형식
         filename_time = datetime.strptime(click_time, "%Y-%m-%d %H:%M:%S").strftime("%Y%m%d_%H%M%S")
-        new_filename = f"{filename_time}_{message}.avi"
+        new_filename = f"{filename_time}_{message}.mp4"
         # 중복 레코딩 방지
         if self.current_filename == new_filename:
             print(f"Skipping recording because filename {new_filename} is already being used.")
